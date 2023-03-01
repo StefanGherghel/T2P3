d5d787da2940ebc29e15d0b2aa5166477f8c4a0d
StefanGherghel <stefan.gherghel@student.tuiasi.ro>
2023-03-01 16:02:54 +0200

    Lab2P1

--- src/Polyglot.java
+++ src/Polyglot.java
@@ -19,34 +19,27 @@ class Polyglot {
     }
 
     //metoda privata pentru evaluarea unei sume de control simple a literelor unui text ASCII, folosind PYTHON
-    private static int SumCRC(String token){
-        //construim un context care ne permite sa folosim elemente din PYTHON
+    private static int[] int_py(){
         Context polyglot = Context.newBuilder().allowAllAccess(true).build();
-        //folosim o variabila generica care va captura rezultatul excutiei functiei PYTHON, sum()
-        //avem voie sa inlocuim anumite elemente din scriptul pe care il construim spre evaluare, aici token provine din JAVA, dar va fi interpretat de PYTHON
-        Value result = polyglot.eval("python", "sum((ord(ch) for ch in '" + token + "'))**2");
-        //utilizam metoda asInt() din variabila incarcata cu output-ul executiei, pentru a mapa valoarea generica la un Int
-        int resultInt = result.asInt();
+        Value result = polyglot.eval("python", "import random\nv=[]\nfor i in range(10):\n\tv.append(random.randint(0,100))\nv");
+
+        int rez[] = new int[10];
+        for (Integer i=0;i<10;i++)
+            rez[i]=result.getArrayElement(i).asInt();
         // inchidem contextul Polyglot
         polyglot.close();
 
-        return resultInt;
+        return rez;
     }
 
     //functia MAIN
     public static void main(String[] args) {
         //construim un context pentru evaluare elemente JS
         Context polyglot = Context.create();
-        //construim un array de string-uri, folosind cuvinte din pagina web:  https://chrisseaton.com/truffleruby/tenthings/
-        Value array = polyglot.eval("js", "[\"If\",\"we\",\"run\",\"the\",\"java\",];");
-        //pentru fiecare cuvant, convertim la upcase folosind R si calculam suma de control folosind PYTHON
-        for (int i = 0; i < array.getArraySize();i++){
-            String element = array.getArrayElement(i).asString();
-            String upper = RToUpper(element);
-            int crc = SumCRC(upper);
-            System.out.println(upper + " -> " + crc);
-        }
-        // inchidem contextul Polyglot
+
+        int res[] = int_py();
+        for (Integer i=0;i<10;i++)
+            System.out.print(res[i]+" ");
         polyglot.close();
     }
 }
