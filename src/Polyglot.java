9139b5bed5e3d0453dff2154f31ef0c9cd775db9
StefanGherghel <stefan.gherghel@student.tuiasi.ro>
2023-03-01 16:04:51 +0200

    Lab2P1

--- src/Polyglot.java
+++ src/Polyglot.java
@@ -4,21 +4,6 @@ import org.graalvm.polyglot.*;
 //clasa principala - aplicatie JAVA
 class Polyglot {
     //metoda privata pentru conversie low-case -> up-case folosind functia toupper() din R
-    private static String RToUpper(String token){
-        //construim un context care ne permite sa folosim elemente din R
-        Context polyglot = Context.newBuilder().allowAllAccess(true).build();
-        //folosim o variabila generica care va captura rezultatul excutiei funcitiei R, toupper(String)
-        //pentru aexecuta instructiunea I din limbajul X, folosim functia graalvm polyglot.eval("X", "I");
-        Value result = polyglot.eval("R", "toupper(\""+token+"\");");
-        //utilizam metoda asString() din variabila incarcata cu output-ul executiei pentru a mapa valoarea generica la un String
-        String resultString = result.asString();
-        // inchidem contextul Polyglot
-        polyglot.close();
-
-        return resultString;
-    }
-
-
     //metoda privata pentru evaluarea unei sume de control simple a literelor unui text ASCII, folosind PYTHON
     private static int[] int_py(){
         Context polyglot = Context.newBuilder().allowAllAccess(true).build();
