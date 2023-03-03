922a1967e9bc769b76ab7c92a05ac6c42b2f5484
StefanGherghel <stefan.gherghel@student.tuiasi.ro>
2023-03-03 13:55:12 +0200

    final

--- src/Polyglot.java
+++ src/Polyglot.java
@@ -9,20 +9,20 @@ import java.util.Arrays;
 //clasa principala - aplicatie JAVA
 class Polyglot {
 
-    private static int[] insert_dataset(){
+    private static int[] insert_numbers_PY(){
         Context polyglot = Context.newBuilder().allowAllAccess(true).build();
-        Value result = polyglot.eval("python", "data=[]\ncitit=1\nfor i in range (10) :\n\tprint(\"Insert: \")\n\tcitit=int(input())\n\tif citit !=0:\n\t\tdata.append(citit)\ndata");
-        int rez[] = new int[10];
-        for (var i=0;i<10;i++)
+        Value result = polyglot.eval("python", "data=[]\ndata.append(int(input(\"Nr de aruncari: \")))\ndata.append(int(input(\"Nr max de castiguri: \")))\ndata");
+        int rez[] = new int[2];
+        for (var i=0;i<2;i++)
             rez[i]=result.getArrayElement(i).asInt();
         polyglot.close();
         return rez;
         // inchidem contextul Polyglot
     }
-    private static void linear_regresion_R(int[]v){
+    private static void bin_distribution_R(int[]v){
         Context polyglot = Context.newBuilder().allowAllAccess(true).build();
         polyglot.getBindings("R").putMember("v",v);
-        Value result = polyglot.eval("R", "x<-c(0,1,2,3,4,5,6,7,8,9)\nplot(x,v)\nabline(lm(x ~ v))");
+        Value result = polyglot.eval("R", "x<-seq(0,v[1],by=1)\ny <- dbinom(x, v[1], prob=0.5)\npng(file = \"dbinom.png\")\nplot(x,y)\ndev.off()\nprint(\"Probabilitatea sa castige de mai putin de nr introsus de ori: \")\nprint(sum(dbinom(0:v[2],v[1],0.5)))\n");
         //int rez[] = new int[10];
         //for (var i=0;i<10;i++)
         //    rez[i]=result.getArrayElement(i).asInt();
@@ -36,13 +36,11 @@ class Polyglot {
     public static void main(String[] args) throws java.io.IOException{
 
         Context polyglot = Context.create();
-        int[] v = insert_dataset();
-        FileWriter myWriter = new FileWriter("./dataset.txt");
-        myWriter.write(Arrays.toString(v));
-        myWriter.close();
+
+        int[]Numere = insert_numbers_PY();
+        bin_distribution_R(Numere);
 
 
-        linear_regresion_R(v);
         polyglot.close();
     }
 }
