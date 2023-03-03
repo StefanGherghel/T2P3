49be1be96ba3fa9fd26f2483782e06af434bff02
StefanGherghel <stefan.gherghel@student.tuiasi.ro>
2023-03-03 13:27:44 +0200

    final

--- src/Polyglot.java
+++ src/Polyglot.java
@@ -19,6 +19,17 @@ class Polyglot {
         return rez;
         // inchidem contextul Polyglot
     }
+    private static void linear_regresion_R(int[]v){
+        Context polyglot = Context.newBuilder().allowAllAccess(true).build();
+        polyglot.getBindings("R").putMember("v",v);
+        Value result = polyglot.eval("R", "x<-c(0,1,2,3,4,5,6,7,8,9)\nplot(x,v)\nabline(lm(x ~ v))");
+        //int rez[] = new int[10];
+        //for (var i=0;i<10;i++)
+        //    rez[i]=result.getArrayElement(i).asInt();
+        polyglot.close();
+        //return rez;
+        // inchidem contextul Polyglot
+    }
 
 
     //functia MAIN
@@ -29,6 +40,9 @@ class Polyglot {
         FileWriter myWriter = new FileWriter("./dataset.txt");
         myWriter.write(Arrays.toString(v));
         myWriter.close();
+
+
+        linear_regresion_R(v);
         polyglot.close();
     }
 }
